0b2b94cfe754936bfcb212e40a76aedf3ee7d6db
wangchengkun <wangck@iccgame.com>
2025-09-29 15:35:21 +0800

    feat: 配置文件

--- config.mjs
+++ config.mjs
@@ -5,29 +5,64 @@ export default {
   },
   "history-person": {
     url: "input/history-person/康熙大帝.mp4",
-    title: "{{千古一帝}}-康熙",
+    title: "三皇五帝之{{燧人氏}}",
+    // 快乐传递者何炅
+    // 国士无双袁隆平
     titleAnimation: "sweep_slow", // 可选值: "flash", "fade", "scale", "slide", "none", "sweep_fast", "sweep_slow", "sweep_pulse", "sweep_rainbow", "sweep_wave", "sweep_laser", "sweep_glow", "sweep_neon", "sweep_electric", "sweep_diamond"
     sectionTitleAnimation: "fade", // 分镜字幕动画效果，可选值同titleAnimation
     sectionTitle: [
-      "1654年\n北京紫禁城\n幼年登基",
-      "1661年\n孝庄太皇太后指导下\n学习为君之道",
-      "1669年\n以摔跤少年\n智擒权臣鳌拜",
-      "1684年\n亲临{{黄河堤坝}}\n视察水利工程",
-      "1696年\n御驾亲征\n奠定{{漠北}}胜局",
-      "1712年\n推行{{仁政}}\n减轻百姓负担",
-      "1722年\n去世\n精神与功业永垂史册",
+      "公元前50000年/0岁\n燧人氏于原始部落降生",
+      "公元前49970年/30岁\n青年燧人氏观察自然现象\n对火产生好奇",
+      "公元前49950年/50岁\n发明钻木取火\n首次主动创造火种",
+      "公元前49945年/55岁\n将取火技术传授给族人",
+      "公元前49940年/60岁\n教民熟食\n提升部落健康水平",
+      "公元前49920年/80岁\n利用火塘\n带领族人建造更宜居的半地穴式房屋",
+      "公元前49900年/100岁\n利用火制作出第一批原始陶器",
+      "公元前49880年/120岁\n发明结绳记事\n为信息传递奠定基础",
+      "公元前49860年/140岁\n晚年燧人氏\n将智慧与领导权交给下一代",
+      "公元前49850年/150岁\n传递希望的火种\n燧人氏去世",
     ],
+    //[
+    // "1974年/0岁\n何炅出生于长沙",
+    // "1992年/18岁\n考入北京外国语大学",
+    // "1995年/21岁\n在综艺《聪明屋》\n饰演大拇哥",
+    // "1998年/24岁\n与李湘一起\n主持《快乐大本营》",
+    // "2004年/30岁\n演唱歌曲《栀子花开》",
+    // "2007年/33岁\n快乐大本营现场\n及时救场",
+    // "2015年/41岁\n导演电影",
+    // "2019年/45岁\n在向往的生活\n与嘉宾围坐聊天",
+    // "2020年/46岁\n线上云录制",
+    // "2024年/50岁\n穿着定制西服的何炅\n从容走过"
+    //]
+
+    //[
+    //  "1930年/0岁\n袁隆平出生于北平",
+    //  "1936年/6岁\n汉口小学\n上学",
+    //  "1939年/9岁\n战争期间\n跟母亲逃难",
+    //  "1949年/19岁\n重庆校园内\n在图书馆苦读",
+    //  "1953年/23岁\n大学试验田边\n专注观察稻穗",
+    //  "1960年/30岁\n安江农校\n在黑板上绘制水稻图谱",
+    //  "1961年/31岁\n安江农校试验田\n发现特殊稻株",
+    //  "1966年/36岁\n海南稻田\n汗水浸透粗布衣裳\n水稻授粉",
+    //  "1973年/43岁\n海南试验基地\n在田间指导助手",
+    //  "1981年/51岁\n国际研讨会现场\n用流利英语演讲",
+    //  "1995年/65岁\n实验室显微镜前\n认真观察",
+    //  "1999年/69岁\n稻田中\n弯腰检查稻穗",
+    //  "2004年/74岁\n颁奖典礼后台\n手握稻穗模型"
+    //  "2011年/81岁\n迪拜沙漠农场\n穿着防晒服查看水稻",
+    //  "2017年/87岁\n青岛海水稻基地\n手捧耐盐碱水稻开怀大笑",
+    //  "2019年/89岁\n生日会场\n与年轻人分享生日蛋糕",
+    //  "2021年/91岁\n金黄的稻浪在风中起伏\n向袁老致敬"
+    //]
     watermark: "@人物传记史",
     "bg-music": "input/history-person/bg-music.mp3",
   },
   "merge-video": {
     urls: [
-      // 示例：可以混合使用本地路径和远程URL
-      // "input/merge-video/video1.mp4",
-      // "input/merge-video/video2.mp4",
-      // "https://example.com/video3.mp4"
+      "https://v9-artist.vlabvod.com/ac0373e9326286f2cb269017a3ca0352/68e36176/video/tos/cn/tos-cn-v-148450/oMWLIhsTRBJbJPgaxRi3Ag0QnDaQyAtbEgfMai/?a=4066&ch=0&cr=0&dr=0&er=0&lr=display_watermark_aigc&cd=0%7C0%7C0%7C0&br=5964&bt=5964&cs=0&ds=12&ft=5QYTUxhhe6BMyqx9j-kJD12Nzj&mime_type=video_mp4&qs=0&rc=Ojc3ZDs1PDllZzlpZzs1NUBpM2w0bXE5cjs1NjczNDM7M0BfNjBhNDU2NV4xXmEyNjM2YSNqaXA2MmRzX2FhLS1kNC9zcw%3D%3D&btag=c0000e00020000&dy_q=1759127241&feature_id=7bed9f9dfbb915a044e5d473759ce9df&l=20250929142721B8E2821AAAB76F4CBBDE",
+      "https://v9-artist.vlabvod.com/f1d3ff87036bab6500fe40ec69aaa8b0/68e3623f/video/tos/cn/tos-cn-v-148450/oMy0DEAsRgEmOhBGTCtLre8IRiGBE6k4PAeey4/?a=4066&ch=0&cr=0&dr=0&er=0&lr=display_watermark_aigc&cd=0%7C0%7C0%7C0&br=5996&bt=5996&cs=0&ds=12&ft=5QYTUxhhe6BMyqDuj-kJD12Nzj&mime_type=video_mp4&qs=0&rc=MzZlZTU1ZWg6ZzxnZzw4O0BpM2Y1bWs5cjQ1NjczNDM7M0AzYGI1M2ItXmMxMS8vMzNhYSNkcGVkMmRzY2FhLS1kNC9zcw%3D%3D&btag=c0000e00018000&dy_q=1759127452&feature_id=7bed9f9dfbb915a044e5d473759ce9df&l=2025092914305206734F9D23ECFE58F38D",
     ],
-    switch: "叠化", // 支持：叠化、淡入淡出、推拉、擦除、无转场
+    switch: "推拉", // 支持：叠化、淡入淡出、推拉、擦除、无转场
   },
   "ai-remove-watermark": {
     url: "output/history-person/1758868423130_10b9525ce467.mp4",
